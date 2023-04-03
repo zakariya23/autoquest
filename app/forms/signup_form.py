bee5f2679da2921c808fcb53ef9b68997ce55e38
root <root@DESKTOP-OI90J59.localdomain>
2023-04-03 12:42:51 -0400

    minor error handling change

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
+import re
 from app.models import User
 
 
 def user_exists(form, field):
-    # Checking if user exists
+    # Check if the email is in the correct format
     email = field.data
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if not re.match(email_regex, email):
+        raise ValidationError('Please enter a valid email address.')
+
+    # Check if the user exists
     user = User.query.filter(User.email == email).first()
     if user:
         raise ValidationError('Email address is already in use.')
